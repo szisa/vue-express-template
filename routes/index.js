c439ca308d6b4394bed36a6830331d3704374f0e
Hancel.Lin <imlinhanchao@gmail.com>
2018-08-01 18:35:55 +0800

    添加通用上传接口。

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.render('index', { title: '首页' });
+router.get('/', function (req, res, next) {
+    next();
 });
 
 module.exports = router;
