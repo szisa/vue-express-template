4f02a78910cf6860ce2b22b976989fc5dfc72dd1
Hancel.Lin <imlinhanchao@gmail.com>
2019-02-26 20:10:30 +0800

    后端接口更改为webpack proxy解决跨域。

--- frontend/libs/util.js
+++ frontend/libs/util.js
@@ -10,11 +10,7 @@ util.title = function(title) {
     window.document.title = title;
 };
 
-const ajaxUrl = env === 'development' ?
-    `http://localhost:${config.base.port}/` :
-    env === 'production' ?
-    '/' :
-    '/';
+const ajaxUrl = '/api/';
 
 util.ajaxUrl = ajaxUrl;
 
